062e0afb25e55edb774608442aff9e3abeae3eb2
Dalue <dalu08lunga@gmail.com>
2022-09-12 06:28:57 -0700

    base

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include<stdio.h>
+
 /**
  * main - Entry point
  *
+ * Description: print numbers of base16 in lowercase
+ *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-  int d;
-  char low;
+	int digit = 48; /*48; decimal rep of 0*/
 
-  for (d = '0'; d <= '9'; d++)
-    putchar(d);
-  for (low = 'a'; low <= 'f'; low++)
-    putchar(low);
-  putchar('\n');
+	while (digit <= 102) /*102; decimal rep of f*/
+	{
+		putchar(digit);
 
-  return (0);
+		/* after 9 we jump till 96; `*/
+		if (digit == 57)
+			digit += 39;
+		++digit;
+	}
+	putchar('\n');
+
+	return (0);
 }
